f93fe2cfd2348ec39f68076ce61527ebc82305d0
luismasuelli <luismasuelli@hotmail.com>
2016-07-26 22:09:52 -0500

    amount class

--- kushki/v1/types.py
+++ kushki/v1/types.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from . import validators
 
 
 class Languages(object):
@@ -19,3 +20,25 @@ class Environments(object):
     TESTING = KushkiEnvironment('PRODUCTION', 'https://uat.aurusinc.com/kushki/api/v1')
     STAGING = KushkiEnvironment('STAGING', 'https://staging.aurusinc.com/kushki/api/v1')
     PRODUCTION = KushkiEnvironment('PRODUCTION', 'https://p1.kushkipagos.com/kushki/api/v1')
+
+
+class Amount(namedtuple('Amount', ('subtotal_iva', 'iva', 'subtotal_iva_0', 'ice'))):
+
+    def to_hash(self):
+        """
+        Valida y devuelve un diccionario con las claves.
+        :return:
+        """
+
+        subtotal_iva = validators.validate_number(self.subtotal_iva, 0, 12, 'El subtotal IVA')
+        subtotal_iva_0 = validators.validate_number(self.subtotal_iva_0, 0, 12, 'El subtotal IVA 0')
+        iva = validators.validate_number(self.iva, 0, 12, 'El IVA')
+        ice = validators.validate_number(self.ice, 0, 12, 'El ICE')
+        total = self.subtotal_iva + self.subtotal_iva_0 + self.iva + self.ice
+        return {
+            'Subtotal_IVA': subtotal_iva,
+            'Subtotal_IVA0': subtotal_iva_0,
+            'IVA': iva,
+            'ICE': ice,
+            'Total_amount': total,
+        }
