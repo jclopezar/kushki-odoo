da227b65407cd4020d2af069be0e92e05b75c58c
luismasuelli <luismasuelli@hotmail.com>
2016-07-26 21:39:25 -0500

    Kushki Environment

--- kushki/v1/enums.py
+++ kushki/v1/enums.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 class Languages(object):
     ES = 'es'
     EN = 'en'
@@ -7,3 +10,12 @@ class Languages(object):
 class Currencies(object):
     USD = 'usd'
     _default = USD
+
+
+KushkiEnvironment = namedtuple('KushkiEnvironment', ('label', 'url'))
+
+
+class Environments(object):
+    TESTING = KushkiEnvironment('PRODUCTION', 'https://uat.aurusinc.com/kushki/api/v1')
+    STAGING = KushkiEnvironment('STAGING', 'https://staging.aurusinc.com/kushki/api/v1')
+    PRODUCTION = KushkiEnvironment('PRODUCTION', 'https://p1.kushkipagos.com/kushki/api/v1')
